f3885560f0dff60f721aeca3d4c358cc797e2b81
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2024-03-21 12:55:36 -0300

    commit

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,17 +1,31 @@
 import React from 'react'
 
 class ClassState extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            error: true,
+        }
+    }
+
     render() {
         return(
             <div>
 
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
 
                 <p>Por favor, escribe el codigo de seguridad.</p>
 
+                { this.state.error &&   ( <p>Error : el codigo es incorrecto</p>) }
+
                 <input  placeholder='Codigo de seguridad' />
 
-                <button> Comprobar </button>
+                <button
+                     onClick={
+                        () => this.setState(prevState => ({error: !prevState.error}) )
+                    }
+                > Comprobar </button>
             </div>
         )
     }
