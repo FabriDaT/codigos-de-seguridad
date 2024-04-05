3c7071e89ddc64aeef2d3efb56de3330ea4a8540
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2024-04-05 12:40:39 -0300

    first

--- src/UseState.js
+++ src/UseState.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function UseState({name}) {
 
   const [error, setError] = useState(true)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => { } , [ ])
+  
 
   return (
     <div>
@@ -12,11 +16,13 @@ function UseState({name}) {
     <p>Por favor, escribe el codigo de seguridad.</p>
 
     { error &&   ( <p>Error : el codigo es incorrecto</p>) }
+    
+    { loading &&   ( <p>Loading...</p>) }
 
     <input  placeholder='Codigo de seguridad' />
 
     <button
-      onClick={ ()=> setError(!error)}
+      onClick={ ()=> setLoading(!loading)}
     > Comprobar </button>
 </div>
   )
